7620d7e5fda839bf8d19d6ce493a3f294012a972
Minglori-l <maldonadoperega@uvm.edu.ve>
2026-06-28 10:04:27 -0400

    Chore: Limpieza de console.logs y formato de app.js

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use('/api/tickets', ticketsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+  
 });
 
 // error handler
@@ -44,6 +45,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+
 });
 
 module.exports = app;
